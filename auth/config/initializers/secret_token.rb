cdcbd5a426d1d30a695caf9283f72651a6a52ba2
Vanessa Pasquet <pasquet.vanessa@gmail.com>
2012-10-25 11:10:21 +0200

    auth from scratch

---
+++ auth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = 'b44ec256968a43bb8fa5a4a292f6b0d4cc3fe0cb74eb76fc4b2be142f2ce92e91fd343a198d82d90f80ad7562353df91281dddf0cd8807965b19a85403a54359'
